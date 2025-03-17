f18858963e3d752f8a31cf79489a1cb8ec7825a3
hoangduc170803 <hoangduc170803@gmail.com>
2025-03-17 20:32:28 +0700

    update project

---
+++ Project/Project/ViewModels/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Project.Helpers;
+using Project.Models;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Project.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set { _email = value; OnPropertyChanged(nameof(Email)); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(nameof(Password)); }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(ExecuteLogin);
+        }
+
+        private void ExecuteLogin(object parameter)
+        {
+            // Kiểm tra rỗng
+            if (string.IsNullOrWhiteSpace(Email) || string.IsNullOrWhiteSpace(Password))
+            {
+                MessageBox.Show("Email hoặc Password không được để trống.", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            using (var context = new SafeDriveCertDbContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Email == Email && u.Password == Password);
+                if (user != null)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    Application.Current.Windows[0]?.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Email hoặc Password không đúng", "Đăng nhập thất bại", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ Project/Project/Views/LoginWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Project.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Phương thức xử lý sự kiện PasswordChanged
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            // Ví dụ: cập nhật Password vào ViewModel
+            if (DataContext is LoginViewModel vm && sender is PasswordBox pwd)
+            {
+                vm.Password = pwd.Password;
+            }
+        }
+    }
+}
